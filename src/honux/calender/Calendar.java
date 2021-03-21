0ccccd570ffa125600fac3bccff558f1ee4a4e30
Cindy Shin <Cindy Shin@owner-PC>
2021-03-21 16:34:38 +0900

    for() to while()

--- src/honux/calender/Calendar.java
+++ src/honux/calender/Calendar.java
@@ -9,7 +9,8 @@ public class Calendar {
 	public int getMaxDayofMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
-	public void printSampleCalendar(){
+
+	public void printSampleCalendar() {
 		System.out.println(" 일 	 월	 화	 수	 목	 금	 토");
 		System.out.println("---------------------------------------------------");
 		System.out.println(" 1 	 2	 3	 4	 5	 6	 7");
@@ -20,16 +21,19 @@ public class Calendar {
 
 	public static void main(String[] args) {
 
-
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		
+		String prompt = "cal> ";
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
-		System.out.println("반복 횟수를 입력하세요.");
-		int repeat = scan.nextInt();
-		for (int i = 0; i < repeat; i++) {
+		int month = 0;
+		while (true) {
 			System.out.println("달을 입력하세요.");
-			int month = scan.nextInt();
+			System.out.print(prompt);
+			month = scan.nextInt();
+			if(month == -1) break;
+			if(month > 12) continue;
 			System.out.println(month + "월은 " + cal.getMaxDayofMonth(month) + "일까지 있습니다.");
 		}
 		System.out.println("Bye~");
